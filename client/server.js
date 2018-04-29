8e6507ccdd69383c260540fcb162357c8ddbff9e
Brian Ginsburg <brian@brianginsburg.com>
2018-04-28 22:52:15 -0700

    Add server and express dependency

---
+++ client/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+
+const server = express();
+// const testPath = '../tests/';
+const testPath = '../handcoded/';
+
+server.get('/tests/:file', function(req, res) {
+  if (/wasm$/.exec(req.params.file)) {
+    fs.readFile(testPath + req.params.file, (err, data) => {
+      if (err === null) {
+        res.set({
+          'Content-Type': 'application/wasm'
+        });
+        res.send(new Buffer(data));
+      } else {
+        res.status(404).send('not found');
+      }
+    });
+  } else {
+    res.sendFile(req.params.file, {
+      'root': testPath
+    });
+  }
+});
+
+server.use('/', express.static('./'));
+
+console.log('server listening on port 8080');
+server.listen(8080);
