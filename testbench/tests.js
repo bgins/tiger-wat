8997a18d2d698b760bbd326566ee2a3c873f4462
Brian Ginsburg <brian@brianginsburg.com>
2018-05-05 08:57:24 -0700

    Update UI and fetching
    Use async await and chain tests together.
    Add tests for each source fetched.
    Add untested indicator icon.
    Add expected results UI section.

--- testbench/tests.js
+++ testbench/tests.js
@@ -5,11 +5,24 @@ const tigerBody = document.getElementById('tiger-body');
 const watHeader = document.getElementById('wat-header');
 const watBody = document.getElementById('wat-body');
 const outputBody = document.getElementById('output-body');
+const expectedBody = document.getElementById('expected-body');
 
 var outputString = '';
+var lastTest = '';
+
+const resetUI = () => {
+    document.getElementById('test-implemented').className = 'fa fa-circle-thin';
+    document.getElementById('test-generated').className = 'fa fa-circle-thin';
+    document.getElementById('test-validated').className = 'fa fa-circle-thin';
+    document.getElementById('test-passes').className = 'fa fa-circle-thin';
+    tigerBody.textContent = '';
+    watBody.textContent = '';
+    expectedBody.textContent = '';
+    outputBody.textContent = '';
+}
 
 // imports has WebAssembly.Memory object and helper functions
-var importObject = {
+const importObject = {
   env: {
     memory: new WebAssembly.Memory({
       initial: 10,
@@ -19,33 +32,91 @@ var importObject = {
   }
 };
 
-const fetchSource = (test, filetype) => {
+
+const fetchSource = (test, filetype, indicator) => {
   return fetch('tests/' + test + '.' + filetype)
     .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      throw test + '.' + filetype + ' not found';
+    })
+    .then(response => {
+      if (indicator !== undefined) {
+        document.getElementById(indicator).className = 'fa fa-check text-success';
+      }
       return response.text();
+    })
+    .then(result => {
+      return {
+        success: true,
+        result: result
+      };
+    })
+    .catch(err => {
+      if (indicator !== undefined) {
+        document.getElementById(indicator).className = 'fa fa-times text-danger';
+      }
+      return {
+        success: false,
+        result: err
+      };
     });
+};
+
+const fetchTiger = async (test) => {
+  tigerHeader.textContent = test + '.tig';
+  var tiger = await fetchSource(test, 'tig', 'test-implemented');
+  tigerBody.textContent = tiger.result;
+  return tiger.success;
+};
+
+const fetchWat = async (test) => {
+  var tigerSuccess = await fetchTiger(test);
+  watHeader.textContent = test + '.wat';
+  if (tigerSuccess === true) {
+    var wat = await fetchSource(test, 'wat', 'test-generated');
+    watBody.textContent = wat.result;
+    return wat.success;
+  } else {
+    return false;
+  }
+};
+
+const fetchExpected = async (test) => {
+  var expected = await fetchSource(test, 'out.bak');
+  return expected.result;
+};
+
+const fetchWasm = async (test) => {
+  var watSuccess = await fetchWat(test);
+  if (watSuccess === true) {
+    WebAssembly.instantiateStreaming(fetch('tests/' + test + '.wasm'), importObject)
+      .then(async wasmObject => {
+        document.getElementById('test-validated').className = 'fa fa-check text-success';
+        wasmObject.instance.exports.main();
+        outputBody.textContent = 'actual:\n' + outputString;
+        var expected = await fetchExpected(test, 'out.bak');
+        expectedBody.textContent = 'expected:\n' + expected;
+        if (expected === outputString) {
+          document.getElementById('test-passes').className = 'fa fa-check text-success';
+        } else {
+          document.getElementById('test-passes').className = 'fa fa-times text-danger';
+        }
+        outputString = '';
+      })
+      .catch(err => {
+        document.getElementById('test-validated').className = 'fa fa-times text-danger';
+        outputBody.textContent = err;
+      });
+  }
+  lastTest = test; // save for run again button
 };
 
 
 function runTest(test) {
-  fetchSource(test, 'tig')
-    .then(tigerSource => {
-      tigerHeader.textContent = test + '.tig';
-      tigerBody.textContent = tigerSource;
-    });
-
-  fetchSource(test, 'wat')
-    .then(watSource => {
-      watHeader.textContent = test + '.wat';
-      watBody.textContent = watSource;
-    });
-
-  // fetch and instantiate wasm then run test
-  WebAssembly.instantiateStreaming(fetch('tests/' + test + '.wasm'), importObject).then(wasmObject => {
-    wasmObject.instance.exports.main();
-    outputBody.textContent = outputString;
-    outputString = '';
-  });
+  resetUI();
+  fetchWasm(test); // start fetching
 }
 
 
@@ -53,12 +124,17 @@ function runTest(test) {
 var tests = ['ints', 'limits', 'add', 'subtract', 'multiply', 'divide'];
 
 jQuery('#test-selection')
-  .children()
-  .children()
-  .children()
+  .find('a')
   .each((index, test) => {
     jQuery(test)
       .on('click', () => {
         runTest(tests[index]);
       });
+  });
+
+jQuery('#run-again')
+  .on('click', () => {
+    if (lastTest !== '') {
+      runTest(lastTest);
+    }
   });
